43f2fb87ce0ea4a8942b43a95fbb5e12d026aac3
Your Name <momenyasser567@gmail.com>
2025-07-05 14:41:44 +0300

    add ProjectDetailView to urlpatterns for project detail retrieval

--- Backend/Django_REST/projects/urls.py
+++ Backend/Django_REST/projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateProjectView
+from .views import CreateProjectView, ProjectDetailView
 
 urlpatterns = [
     path('create/', CreateProjectView.as_view(), name='create-project'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
 ]
