b6074831104ee7e7702ba19f7ff4c4166ea61f7a
Ahmedmanso4r <UGS.141184@ci.suez.edu.eg>
2025-07-08 15:09:54 +0300

    feat: Update Django settings for CORS_ALLOWED_ORIGINS

--- Backend/Django_REST/Django_REST/settings.py
+++ Backend/Django_REST/Django_REST/settings.py
@@ -150,6 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5176",
     "http://127.0.0.1:5176",
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
